452371b0beecb757970edf77ca47a7d1fedd8fc6
Patryk Owczarek <Patryk.Owczarek@rockwellautomation.com>
2021-05-19 08:52:36 +0200

    Format the html and js files

--- js/item_list.js
+++ js/item_list.js
@@ -1,28 +1,28 @@
-function generateItem(number, photo_url, item_name, item_price, item_rating){
+function generateItem(number, photo_url, item_name, item_price, item_rating) {
 	var html = "";
-	html +=	'<li>';
-	html +=	'<div class="item item'+ number +'">';
-	html +=	'<img src="' + photo_url + '" class="item_photo">';
-	html +=	'<h4 class="item_name">' + item_name + '</h4>';
-	html +=	'<h4 class="item_price">Cena za sztukę: ' + item_price +'</h4>';
-	html +=	'<h4 class="item_rating">Ocena: ' + item_rating +'</h4>';
-	html +=	'<img src="img/add_to_cart.png" class="add_to_cart">';
-	html +=	'</div>';
-	html +=	'</li>';
+	html += '<li>';
+	html += '<div class="item item' + number + '">';
+	html += '<img src="' + photo_url + '" class="item_photo">';
+	html += '<h4 class="item_name">' + item_name + '</h4>';
+	html += '<h4 class="item_price">Cena za sztukę: ' + item_price + '</h4>';
+	html += '<h4 class="item_rating">Ocena: ' + item_rating + '</h4>';
+	html += '<img src="img/add_to_cart.png" class="add_to_cart">';
+	html += '</div>';
+	html += '</li>';
 
 	return html;
 }
 
 window.onload = readCategoriesFromUrlQuery;
 
-function readCategoriesFromUrlQuery(){
+function readCategoriesFromUrlQuery() {
 	const urlParams = new URLSearchParams(window.location.search);
 	const myParam = urlParams.get('category');
 	var el = document.querySelector("#" + myParam);
 	el.click();
 }
 
-function filter(){
+function filter() {
 	var bud = document.getElementById("bud");
 	var ogr = document.getElementById("ogr");
 	var tec = document.getElementById("tec");
@@ -31,41 +31,41 @@ function filter(){
 	var laz = document.getElementById("laz");
 	var ele = document.getElementById("ele");
 
-	if (mie.checked == true){
-		$.getJSON('https://mirbud-restapi.herokuapp.com/api/item/categories/meble', function(data){
+	if (mie.checked == true) {
+		$.getJSON('https://mirbud-restapi.herokuapp.com/api/item/categories/meble', function (data) {
 			var html = "";
 			for (var i = 0; i < data.length; i++) {
 				html += generateItem(data[i].id, data[i].zdjecia, data[i].nazwa, data[i].cenaSprzedazy, data[i].ocena);
 			}
 			document.getElementById("lista").innerHTML += html;
-		});	
+		});
 	}
 }
 
-const data = { 
+const data = {
 	"email": "email@osyslubissacpalska.com",
-  "haslo": "haslo",
-  "imie": "imie",
-  "kodPocztowy": "kod",
-  "miejscowosc": "miejscowosc",
-  "nazwisko": "nazwisko",
-  "nrTelefonu": "123456789",
-  "ulicaNrDomu": "ulica"
+	"haslo": "haslo",
+	"imie": "imie",
+	"kodPocztowy": "kod",
+	"miejscowosc": "miejscowosc",
+	"nazwisko": "nazwisko",
+	"nrTelefonu": "123456789",
+	"ulicaNrDomu": "ulica"
 };
 
-function doregistration(){
+function doregistration() {
 	fetch('https://mirbud-restapi.herokuapp.com/api/clients/registration', {
-  	method: 'POST', // or 'PUT'
-  	headers: {
-    	'Content-Type': 'application/json',
-  	},
-  		body: JSON.stringify(data),
+		method: 'POST', // or 'PUT'
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
 	})
 		.then(response => response.json())
 		.then(data => {
-	  	console.log('Success:', data);
-	})
+			console.log('Success:', data);
+		})
 		.catch((error) => {
-	  	console.error('Error:', error);
-	});
+			console.error('Error:', error);
+		});
 }
--- js/recommended.js
+++ js/recommended.js
@@ -1,22 +1,22 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$.ajax({
 		url: "https://mirbud-restapi.herokuapp.com/api/item/recommended/"
-	}).then(function(data){
+	}).then(function (data) {
 		var i;
-		for (i = 0; i < 6; i++){
+		for (i = 0; i < 6; i++) {
 			var searchedClassName = ".item" + i + " h4.item_name";
 			$(searchedClassName).append(data[i].nazwa);
 		}
-		for (i = 0; i < 6; i++){
+		for (i = 0; i < 6; i++) {
 			var searchedClassName = ".item" + i + " h4.item_price";
 			$(searchedClassName).append(data[i].cenaSprzedazy + "zł");
 		}
-		for (i = 0; i < 6; i++){
+		for (i = 0; i < 6; i++) {
 			var searchedClassName = ".item" + i + " img.item_photo";
 			$(searchedClassName).attr("src", data[i].zdjecia);
 		}
-		for (i = 0; i < 6; i++){
+		for (i = 0; i < 6; i++) {
 			var searchedClassName = ".item" + i + " h4.item_rating";
 			$(searchedClassName).append(data[i].ocena + "/5");
 		}
